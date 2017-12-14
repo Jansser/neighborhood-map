5c36fa63b0684dd332cd8e1f5062f2ede65743aa
Jansser <jansser_costa@hotmail.com>
2017-12-14 11:21:40 -0300

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Neighborhood Map
+
+The Neighborhood Map or the Neighbor"Burger" Map is a project to list all "burger places" around my location using google maps API.
+Basically the user can view the marker of those places on the map, a list view with a filter input and more information about those places provided by foursquare API. The project was developed using KnockoutJS to Full Stack Web Developer Nanodegree program.
+
+### How to run
+
+* Download the repository or clone ```git clone https://github.com/Jansser/nanodegree-catalog-items.git```
+* Open the **index.html** file.
+* Now you can click on a map marker or item on sidebar view and see more about the locations.
--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/*
+* Locations dataset
+*/
 var data = {
   locations: ko.observableArray([
     new Location('Xoxo Burguer',                  { lat: -12.9668469, lng: -38.4653078 }),
@@ -9,49 +12,74 @@ var data = {
   ])
 };
 
+/**
+* @description Represents a Location
+* @constructor
+* @param {string} title - The title of the location.
+* @param {object} coordinates - Object with the lat and long of the object.
+*/
 function Location(title, coordinates) {
-  this.title = title;
-  this.coordinates = coordinates;
+  let self = this;
+  self.title = title;
+  self.coordinates = coordinates;
 
+  /*
+  * Loads the foursquare API data
+  */
+  self.getFoursquareData = function () {
+    const FORSQUARE_CLIENT_ID = 'XU0HWTQITZBSMD2HJDPOKQTYRFBVWPCY4QAM03KG0LKVVSS2';
+    const FORSQUARE_CLIENT_SECRET = 'KWFQ22SUYMKL4JYPKJMT5CKHZRNZJPWBRLN5FDL52FKGO5MU';
 
-  this.getFoursquareData = function () {
     let url = 'https://api.foursquare.com/v2/venues/search?v=20171112' +
-      '&ll=' + this.coordinates.lat + ',' + this.coordinates.lng +
-      '&query=' + this.title +
-      '&client_id=' + viewModel.config.FORSQUARE_CLIENT_ID +
-      '&client_secret=' + viewModel.config.FORSQUARE_CLIENT_SECRET +
+      '&ll=' + self.coordinates.lat + ',' + self.coordinates.lng +
+      '&query=' + self.title +
+      '&client_id=' + FORSQUARE_CLIENT_ID +
+      '&client_secret=' + FORSQUARE_CLIENT_SECRET +
       '&limit=1';
 
     $.getJSON(url).done(function(response) {
       let venues = response.response.venues;
 
-      info = venues[0];
+      if(venues && venues.length) {
+        info = venues[0];
 
-      console.log( info.id );
-      console.log(info.contact);
-      console.log(info.location.formattedAddress.join(', '));
+        if(info) {
+          self.address  = info.location.formattedAddress.join(', ');
+          self.category = info.categories[0].shortName;
+        }
+      }
     }).fail(function () {
-      //show error message
-      //test by inserting error in url
-      console.log('oops');
+      viewModel.showError('Sorry, we couldn\'t load foursquare data');
     });
   }
+
+  this.getFoursquareData();
 }
 
 var viewModel = {
-  config: {
-    FORSQUARE_CLIENT_ID: 'XU0HWTQITZBSMD2HJDPOKQTYRFBVWPCY4QAM03KG0LKVVSS2',
-    FORSQUARE_CLIENT_SECRET: 'KWFQ22SUYMKL4JYPKJMT5CKHZRNZJPWBRLN5FDL52FKGO5MU'
-  },
-
   query: ko.observable(""),
   map,
+  infoWindow: null,
   initMap: function () {
     map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: -12.9730401, lng: -38.5023039 },
-      zoom: 16
+      zoom: 16,
+      styles: [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},
+        {"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},
+        {"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels",
+        "stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry",
+        "stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels",
+        "stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry",
+        "stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels",
+        "stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry",
+        "stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels",
+        "stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all",
+        "stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all",
+        "stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}
+      ]
     });
 
+    this.infoWindow = new google.maps.InfoWindow();
     this.createMarkers();
   },
 
@@ -64,6 +92,7 @@ var viewModel = {
         map: map,
         title: location.title,
         animation: google.maps.Animation.DROP,
+        icon: 'images/burger.png',
         id: index
       });
 
@@ -79,23 +108,38 @@ var viewModel = {
   },
 
   showInfoWindow: function (location) {
-    let infoWindow = new google.maps.InfoWindow()
+    let infoWindow = this.infoWindow;
     let marker = location.marker;
 
     if(infoWindow.marker != marker) {
+      infoWindow.setContent('');
       infoWindow.marker = marker;
-      infoWindow.setContent('<div>' + marker.title + '</div>');
 
+      let content =
+      '<div class="card border-0">'
+        + '<div class="card-body">'
+          + '<h5 class="card-title">' + marker.title + '</h5>';
+
+          if(location.category) {
+            content = content + '<h6 class="card-subtitle mb-2 text-muted">' + location.category + '</h6>'
+            + '<ul class="list-group list-group-flush">'
+              + '<li class="list-group-item"><strong>Address</strong></li>'
+              + '<li class="list-group-item text-left">' + location.address + '</li>'
+            + '</ul>'
+            + '<img src="images/foursquare_logo.png" class="float-right">';
+          }
+
+        content = content + '</div>'
+      + '</div>';
+
+      infoWindow.setContent(content);
       infoWindow.open(viewModel.map, marker);
     }
-
-    location.getFoursquareData();
   },
 
   highlightLocation: function (location) {
     let marker = location.marker;
 
-    //TODO Change color
     marker.setAnimation(google.maps.Animation.BOUNCE);
 
     setTimeout(function () {
@@ -113,9 +157,8 @@ var viewModel = {
     });
   },
 
-  //TODO: test it blocking the google site on hosts file
   showError: function(message) {
-    alert(message);
+    swal('Oops...', message,'error');
   }
 };
 
@@ -124,6 +167,10 @@ var viewModel = {
  */
 viewModel.filteredLocations = ko.computed(function () {
   let query = this.query().toLowerCase();
+
+  if(viewModel.infoWindow) {
+    viewModel.infoWindow.close();
+  }
 
   if(query) {
     return ko.utils.arrayFilter(data.locations(), function (location) {
